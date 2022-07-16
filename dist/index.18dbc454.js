b801963720d1c480923a90fc1834a7c6a0aba5ed
sidney <etreailleurs@hotmail.fr>
2022-07-16 13:42:44 +0200

    added nav links to sections

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -536,7 +536,7 @@ theme.addEventListener('click', ()=>{
 });
 
 },{}],"2xjmL":[function(require,module,exports) {
-const about = document.getElementById('about');
+const about = document.getElementById('aboutText');
 const text = "Après de nombreuses péripéties, je décide de m'établir dans la belle contrée du web developpement en 2018. Ce qui devait être un court apprentissage lié à un projet de site de langue, se transforma en véritable passion. Autodidacte enthousiaste, j'ai à coeur de donner le meilleur de moi-même pour chacune des missions qui me sont confiées.";
 let i = 0;
 function typeText() {
--- src/js/_about.js
+++ src/js/_about.js
@@ -1,4 +1,4 @@
-const about = document.getElementById('about')
+const about = document.getElementById('aboutText')
 
 const text =
 	"Après de nombreuses péripéties, je décide de m'établir dans la belle contrée du web developpement en 2018. Ce qui devait être un court apprentissage lié à un projet de site de langue, se transforma en véritable passion. Autodidacte enthousiaste, j'ai à coeur de donner le meilleur de moi-même pour chacune des missions qui me sont confiées."
